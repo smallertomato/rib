1dd0e8ef0a777658b3cd551ae8e051d41cb01363
Chao Liu <chao.c.liu@intel.com>
2012-08-24 05:01:15 +0800

    [Property View] Reduce refresh the property view
    When property change, we should just set the new value of the
    property instead of refresh (refresh recreating whole DOM
    including event binding is a very heavy action which we should
    decrease as possible)
    
    Another reason: widget of property view  will lose focus if we recreate
    the whole DOM

--- src/js/views/property.js
+++ src/js/views/property.js
@@ -80,11 +80,18 @@
             widget.refresh(event,widget);
         },
 
+        _setProperty: function(property, value) {
+            var viewId = property + '-value';
+            this.element.find("#" + viewId).val(value);
+        },
+
         _modelUpdatedHandler: function(event, widget) {
             widget = widget || this;
-            if (event && (event.type === "propertyChanged" &&
-                        event.node.getType() === 'Design')) {
-                return;
+            if (event && event.type === "propertyChanged") {
+                if (event.node.getType() === 'Design') {
+                    return;
+                }
+                widget._setProperty(event.property, event.newValue);
             } else {
                 widget.refresh(event,widget);
             }
